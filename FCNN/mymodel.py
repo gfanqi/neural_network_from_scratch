ccc14fed7fec54fe243b76e66265f4527dab310b
gfanqi <gfanqi@qq.com>
2019-12-16 15:40:45 +0800

    完成功能的分割和测试

---
+++ FCNN/mymodel.py
@@ -0,0 +1,41 @@
+from FCNN.layers import Layers
+from FCNN.models import Model
+
+
+class Mymodel(Model):
+	def __init__(self):
+		'''
+		初始化层
+		'''
+		super().__init__()
+		self.layer1 = Layers.Dense(2)
+		self.layer2 = Layers.Dense(2)
+		self.layer3 = Layers.Dense(3)
+		self.layer4 = Layers.Dense(1)
+
+	def call(self, data):
+		'''
+		前向传播逻辑
+		:param data:
+		:return:
+		'''
+		x = self.layer1(data)
+		x = self.layer2(x)
+		x = self.layer3(x)
+		output = self.layer4(x)
+		return output
+
+
+	def backproandrenew(self, lossfunc=None, learning_rate=None):
+		'''
+		反向传播算法，更新参数
+		:param lossfunc:
+		:param learning_rate:
+		:return:
+		'''
+		mylossfunc = lossfunc(self.y, self.layer4.result)
+		last_grad = mylossfunc.grad()
+		last_grad = self.layer4.backpropagation(last_grad)
+		last_grad = self.layer3.backpropagation(last_grad)
+		last_grad = self.layer2.backpropagation(last_grad)
+		last_grad = self.layer1.backpropagation(last_grad)
--- FCNN/从零实现一个全连接神经网络.py
+++ FCNN/从零实现一个全连接神经网络.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from FCNN.losses import Losses
-from FCNN.models import Model
+from FCNN.mymodel import Mymodel
 from utils.makefigure import show_line_and_data
 
 
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     y = np.mat(np.vstack((y1,y2)))
 
     show_line_and_data(data, y)
-    mymodel = Model()
-    mymodel.fit(data, y,epochs=1000, lossfunc=Losses.ErrorSqure,learning_rate=0.5)
+    mymodel = Mymodel()
+    mymodel.fit(data, y,epochs=100, lossfunc=Losses.ErrorSqure,learning_rate=0.5)
     test_data = np.random.random(size = (10000,2))*3
     show_line_and_data(test_data,mymodel.predict(test_data))
     print(mymodel.predict(data))
